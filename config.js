0ca82d9373a8190eb21de93145765d9ab0e042e8
scott_diallo <diallot@me.com>
2016-09-03 23:29:27 +0000

    app complete

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+                              'mongodb://scott.diallo6@gmail.com:<Univa2011@>@ds019976.mlab.com:19976/mongod-thinkful-course':
+                            'mongodb://localhost/mongod-thinkful-course'
+
+);
 exports.PORT = process.env.PORT || 8080;
